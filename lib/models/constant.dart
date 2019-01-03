98a6479e40d86c1796a4ce1c25a1a44a7ee41455
wingkwong <wingkwong.code@gmail.com>
2019-01-03 22:38:27 +0800

    #6 added constants for registartion screen

--- lib/models/constant.dart
+++ lib/models/constant.dart
@@ -1,1 +1,6 @@
-const String _app_name = "我地.佳招";
+const String APP_NAME = "我地.佳招";
+const String SMS_CODE_DIALOG_TITLE = "Enter SMS Code";
+const String REG_BUTTON_TEXT = "Register";
+const String REG_USERNAME_HINT_TEXT = "Enter Username";
+const String REG_PHONE_NUMBER_HINT_TEXT = "Enter Phone number";
+const String REG_ADDRESS_HINT_TEXT = "Home / Office Address (Optional)";
