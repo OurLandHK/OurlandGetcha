4d032526cad578ab9bf39c51098f8614bfdf27db
wingkwong <wingkwong.code@gmail.com>
2019-01-07 00:06:32 +0800

    #6 added GestureDetector & image picker get image function

--- lib/pages/registration_screen.dart
+++ lib/pages/registration_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:ourland_native/models/constant.dart';
 import 'package:ourland_native/services/user_service.dart';
 
@@ -20,7 +22,15 @@ class _PhoneAuthenticationScreenState extends State<PhoneAuthenticationScreen> {
   String avatarUrl = 'assets/images/default-avatar.jpg';
   String smsCode;
   String verificationId;
+  File avatarImage;
 
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      avatarImage = image;
+    });
+  }
 
   void verify(context) {
     if(this.username == null) {
@@ -119,14 +129,17 @@ class _PhoneAuthenticationScreenState extends State<PhoneAuthenticationScreen> {
   }
 
   renderAvatar() {
-    return Container(
-      width: 80.0,
-      height: 80.0,
-      decoration: new BoxDecoration(
-          shape: BoxShape.circle,
-          image: new DecorationImage(
-              fit: BoxFit.fill,
-              image: new ExactAssetImage(DEFAULT_AVATAR_IMAGE_PATH)
+    return GestureDetector(
+      onTap: getImage,
+      child: Container(
+          width: 80.0,
+          height: 80.0,
+          decoration: new BoxDecoration(
+              shape: BoxShape.circle,
+              image: new DecorationImage(
+                fit: BoxFit.fill,
+                image: new ExactAssetImage(DEFAULT_AVATAR_IMAGE_PATH),
+              )
           )
       )
     );
