cc03c30881351a35afb03268aaecedf8c5d59a0d
khmak3 <pegasus.mak@gmail.com>
2019-01-20 00:12:50 +0800

    Prepare for marker

--- lib/pages/chat_map.dart
+++ lib/pages/chat_map.dart
@@ -5,9 +5,13 @@ import 'package:geolocator/geolocator.dart';
 import 'package:ourland_native/widgets/map/index.dart';
 
 class ChatMap extends StatefulWidget {
-  final Position mapCenter;
+  Position mapCenter;
 
   ChatMap({Key key, @required this.mapCenter}) : super(key: key);
+  void updateCenter(Position _mapCenter) {
+    print('ChatMap called ${_mapCenter}');
+    this.createState().updateCenter(_mapCenter);
+  }
 
   @override
   _ChatMapState createState() => new _ChatMapState(mapCenter: this.mapCenter);
@@ -16,7 +20,7 @@ class ChatMap extends StatefulWidget {
 // https://pub.dartlang.org/packages/geolocator
 
 class _ChatMapState extends State<ChatMap> {
-  final Position mapCenter;
+  Position mapCenter;
   
   @override
   _ChatMapState({Key key, @required this.mapCenter});
@@ -25,11 +29,20 @@ class _ChatMapState extends State<ChatMap> {
     super.initState();
   }
 
+  void updateCenter(Position _mapCenter) {
+    print('ChatMapState called ${_mapCenter}');
+    this.mapCenter = _mapCenter;
+  }
+
   @override
   Widget build(BuildContext context) {
+    /*
+    Widget rv = new GoogleMapWidget(this.mapCenter.latitude, this.mapCenter.longitude);
+    return rv;
+    */
     List<Widget> widgets;
     widgets = [
-        new GoogleMapWidget(this.mapCenter.latitude, this.mapCenter.longitude)
+        GoogleMapWidget(this.mapCenter.latitude, this.mapCenter.longitude)
       ];
     return new Column(
       crossAxisAlignment: CrossAxisAlignment.start,
