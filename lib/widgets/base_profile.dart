c551c95a735173c328de5358e02079164fabe537
khmak3 <pegasus.mak@gmail.com>
2019-03-23 17:36:35 +0800

    #75 add base_profile to handle all profile display

---
+++ lib/widgets/base_profile.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:ourland_native/models/user_model.dart';
+import 'package:ourland_native/widgets/image_widget.dart';
+import 'package:ourland_native/models/constant.dart';
+
+class BaseProfile extends StatelessWidget {
+  final User user;
+
+  BaseProfile(
+      {Key key,
+      @required this.user,})
+      : super(key: key);
+
+
+  Widget build(BuildContext context) {
+    String imageUrl = (this.user.avatarUrl != null) ? this.user.avatarUrl : 'assets/images/default-avatar.jpg';
+    Widget imageWidget = new ImageWidget(width: 50, height: 50, imageUrl: imageUrl);
+    Widget rv = Column(
+      children: <Widget>[
+        Material(
+          child: imageWidget,
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+          clipBehavior: Clip.hardEdge,
+        ),
+        Container(
+          child: Text(
+            this.user.username,
+            style: TextStyle(color: primaryColor),
+          ),
+          alignment: Alignment.center,
+          margin: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
+        ),
+      ]
+    );
+    return rv;
+  }
+}
+
+
